05d694a06c9f123c124df141ce0a3c6b3fe8969d
alexandre siaux <alexandresiaux@gmail.com>
2018-01-14 19:59:21 +0100

    au cas ou

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: asiaux <asiaux@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/17 04:15:35 by asiaux       #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/09 14:22:54 by asiaux      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/14 19:58:35 by asiaux      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,12 +16,14 @@
 static t_list			*ft_mod(t_list *lst, char *str)
 {
 	t_list	*lsttmp;
+
 	str = ft_strcat((char *)lst->content, str);
 	lsttmp = ft_lstnew(str, ft_strlen(str));
 	free(lst->content);
 	lst->content = NULL;
 	lst->content_size = 0;
 	free (lst);
+//	lst = ft_lstnew(str, ft_strlen(str));
 	return (lsttmp);
 }
 
@@ -40,12 +42,7 @@ static t_gnl			*ft_gnlnew(int fd, t_list *lst)
 		return (NULL);
 	gnl->fd = fd;
 	gnl->lst = lst;
-/*	while (gnl->lst)
-	{
-		dprintf(1,"gnl ligne: %s\n",gnl->lst->content);
-		gnl->lst = gnl->lst->next;
-	}
-*/	return (gnl);
+	return (gnl);
 }
 
 static t_list			*ft_fill_lst(const int fd)
@@ -61,9 +58,9 @@ static t_list			*ft_fill_lst(const int fd)
 	while ((ret = read(fd, buff, BUFF_SIZE)))
 	{
 		buff[ret] = '\0';
-		if (*buff == '\n' && lst->next)
-			lst = ft_seek_n_fill(&lst->next, buff + 1, ret, 0);
-		else
+	//	if (*buff == '\n' && lst->next)
+	//		lst = ft_seek_n_fill(&lst->next, buff + 1, ret, 0);
+	//	else
 			lst = ft_seek_n_fill(&lst, buff, ret, 0);
 	}
 	tmp = head->next;
@@ -101,14 +98,14 @@ t_list			*ft_seek_n_fill(t_list **lst, char *buff, int ret, int size)
 	}
 	if (*buff && !size)
 	{
-		if ((*lst)->next && (str = ft_strsub(buff, 0, ret)))
-		{
+		if ((*lst)->next/* && (str = ft_strsub(buff, 0, ret))*/)
+//		{
 //			str = ft_strcat((char *)(*lst)->next->content, str);
 //			free((*lst)->next);
 //			ft_lstdelone(&(*lst)->next, ft_del);
 //			(*lst)->next = ft_lstnew(str, ft_strlen(str));
-			(*lst)->next = ft_mod((*lst)->next, str);
-		}
+			(*lst)->next = ft_mod((*lst)->next, /*str*/buff);
+//		}
 		else if (!(*lst)->next)
 			(*lst)->next = ft_lstnew(ft_strsub(buff, 0, ret), ft_strlen(buff));
 	}
@@ -126,21 +123,22 @@ int						get_next_line(const int fd, char **line)
 		return (-1);
 	if (!head) /*[1]creation premiere liste*/
 		head = ft_gnlnew(fd, ft_fill_lst(fd));
-	if ((tmp = head) &&tmp->fd != fd) /*[2]recherche fd et si non trouver creation next liste*/
+	if ((tmp = head) && tmp->fd != fd) /*[2]recherche fd et si non trouver creation next liste*/
 	{
 		while (tmp->nextgnl && tmp->fd != fd)
 			tmp = tmp->nextgnl;
 		if (tmp->fd != fd && !tmp->nextgnl)
+		{
 			tmp->nextgnl = ft_gnlnew(fd, ft_fill_lst(fd));
-		if(tmp->nextgnl)
 			tmp = tmp->nextgnl;
+		}
 	}
 	if (tmp->lst && (*line = ft_strdup(tmp->lst->content)))
 	{
 		*(*line + tmp->lst->content_size) = '\0';
 		lsttmp = tmp->lst->next;
-//		free(tmp->lst);
-		ft_lstdelone(&tmp->lst, ft_del);
+		free(tmp->lst);
+//		ft_lstdelone(&tmp->lst, ft_del);
 		tmp->lst = lsttmp;
 		return (1);
 	}
